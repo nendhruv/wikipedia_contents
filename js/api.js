c85b7ebe616cf1c5fe528b42c8b79ed3fdb0eb39
Dhruv Nenwani <nendhruv@gmail.com>
2020-02-08 21:51:06 +0530

    project setup

---
+++ js/api.js
@@ -0,0 +1,10 @@
+export default class Map {
+    constructor() {
+        this.baseUrl = 'wikipedia.org/api/rest_v1/page/metadata/'
+        this.fetchData();
+    }
+
+    fetchData() {
+        console.log(baseUrl);
+    }
+}
